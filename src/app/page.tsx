ecbda6a443f1fb6ec4a9b14240b2e499ac430941
Sajan Rajbhandari <sazanrjb@gmail.com>
2024-01-22 21:47:42 +0545

    Shift assistant page to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,133 @@
+'use client';
+
+import { useState } from 'react';
 import Image from 'next/image';
+import brandLogo from '@/asset/brand-logo.svg';
+import { ChatResponseSchema } from '@/types/ai';
 
-export default function Home() {
+export default function AssistantPage() {
+  const [message, setMessage] = useState('');
+  const [result, setResult] = useState(ChatResponseSchema.parse({}));
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleInputChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleInputSubmit = async (event) => {
+    setIsLoading(true);
+    fetch(`/api/ask?message=${message}`, {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((data) => setResult(ChatResponseSchema.parse(data.data)))
+      .catch((err) => console.error(err))
+      .finally(() => setIsLoading(false));
+
+    event.preventDefault();
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex min-h-[600px] flex-1 flex-col align-middle p-6 sm:px-6 lg:px-8 py-18">
+      <div className="flex flex-col-reverse items-center justify-center">
+        <Image src={brandLogo} alt="Brand Logo" width={150} height={150} />
+      </div>
+
+      <form className="flex flex-row items-center justify-center mt-16">
+        <div className="bg-white relative flex w-full hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-6 py-4 items-center sm:max-w-xl lg:max-w-2xl">
+          <input
+            type="text"
+            className="flex-grow focus:outline-none bg-transparent"
+            placeholder="Where should I go to make my passport ?"
+            onChange={handleInputChange}
+          />
+
+          <button
+            type="submit"
+            className="w-[54px] h-[54px] absolute end-0 flex align-middle rounded-full bg-christmasGreen-700 px-4 py-4 text-lg font-semibold text-gray-100 shadow-sm ring-1 ring-inset ring-slate-500 hover:bg-slate-500 bg-slate-600 disabled:bg-slate-400 disabled:ring-slate-400"
+            disabled={!message || isLoading}
+            onClick={handleInputSubmit}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+              className="text-white"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+            <span className="sr-only">Search</span>
+          </button>
         </div>
-      </div>
+      </form>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {isLoading ? (
+        <p className="text-center mt-8">Loading...</p>
+      ) : (
+        !!Object.values(result).length && (
+          <div className="flex flex-row items-center justify-center mt-20 max-w-md mx-auto">
+            <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow">
+              <div className="rounded-tl-lg rounded-tr-lg sm:rounded-tr-none group relative bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-christmasGreen-700">
+                {result.feedback ? (
+                  <div className="mt-4">
+                    <p className="mt-2 text-sm text-gray-500">
+                      {result.feedback}
+                    </p>
+                  </div>
+                ) : (
+                  <div>
+                    <b>{result.name}</b>
+                    <p className="mt-2 text-sm text-gray-500">
+                      {result.summary}
+                    </p>
+                    <p className="mt-2 text-sm text-gray-500">{result.about}</p>
+                    {result.info?.length && (
+                      <ul className="text-sm mt-4 text-gray-500 ">
+                        {result.info.map(({ location, phone_number }) => (
+                          <>
+                            <li className="bg-gray-100 p-4 mt-2">
+                              <div>
+                                <b>Location: </b>
+                                {location}
+                              </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+                              <div>
+                                <b>Phone Number: </b>
+                                {phone_number}
+                              </div>
+                            </li>
+                          </>
+                        ))}
+                      </ul>
+                    )}
+                    {result.website && (
+                      <>
+                        <div className="mt-4 text-sm text-gray-500">
+                          <b>Website: </b>
+                          <a
+                            href={result.website}
+                            target="_blank"
+                            className="text-sm mt-2 text-blue-500"
+                          >
+                            {result.website}
+                          </a>
+                        </div>
+                      </>
+                    )}
+                  </div>
+                )}{' '}
+              </div>
+            </div>
+          </div>
+        )
+      )}
+    </div>
   );
 }
